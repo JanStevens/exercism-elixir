05d140bbc553a075c23816fac117e4fbfa6a2e40
Jan Stevens <jan@playpass.be>
2016-11-11 17:09:17 +0100

    Added solutions for anagram

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,6 +4,26 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
+    candidates
+      |> Enum.filter(fn(x) -> anagram?(base, x) end)
+  end
 
+  defp anagram?(base, candidate) do
+    unify_string(base) != unify_string(candidate) &&
+    (base |> to_sorted_word) === (candidate |> to_sorted_word)
+  end
+
+  # Sorting is the real hack here, this makes detecting very easy
+  defp to_sorted_word(word) do
+    word
+      |> unify_string
+      |> String.graphemes
+      |> Enum.sort
+  end
+
+  defp unify_string(word) do
+    word
+      |> String.downcase
+      |> String.trim
   end
 end
